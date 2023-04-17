f4c8e88fbc3743a2661d07538cd5ac430c98631e
Hasan Kakeh <77812447+hasanKakeh@users.noreply.github.com>
2023-04-17 15:30:58 +0200

    add component

--- projects/ngx-lite-video-prototype/src/app/app.module.ts
+++ projects/ngx-lite-video-prototype/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,7 @@ import { NgxLiteVideoComponent } from 'projects/ngx-lite-video/src/public-api';
 
 @NgModule({
   declarations: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [BrowserModule, AppRoutingModule, NgxLiteVideoComponent],
   providers: [],
   bootstrap: [AppComponent],
