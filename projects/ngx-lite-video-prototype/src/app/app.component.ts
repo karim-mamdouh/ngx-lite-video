885777222b0944237d747d8f856015f528aa43ec
Hasan Kakeh <77812447+hasanKakeh@users.noreply.github.com>
2023-04-18 20:49:10 +0200

    make video id changeable

--- projects/ngx-lite-video-prototype/src/app/app.component.ts
+++ projects/ngx-lite-video-prototype/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-lite-video-prototype';
+  youtubeId = "6drfp_3823I"
+  youtubeTitle="London Grammar - Strong [Official Video]"
+  vimeoId = "786570322"
+  change() {
+    this.youtubeId = "tHFMD2f__iA";
+    this.youtubeTitle="الفراقية لابن زريق البغدادي | أحمد بن عبدالعزيز النفيس"
+    this.vimeoId = '818118134'
+  }
 }
